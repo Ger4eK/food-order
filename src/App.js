c3cf8aee91311ca49e20ea4773a0bb3c6447a81c
Ger4eK <ger4ek228@gmail.com>
2022-03-11 14:46:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
-//!
+
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
