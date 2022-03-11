a1a9c70e3891255f5cc39db7df4b9c3c3b58e757
Ger4eK <ger4ek228@gmail.com>
2022-03-11 16:54:44 +0200

    add bump animation

--- src/components/Layout/HeaderCartButton/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.jsx
@@ -1,17 +1,42 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../../store/cart-context';
 import CartIcon from '../../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  //! Here I can just refer to items and pass items as a dependency, so that not the entire context, but only the items array is a dependency for this effect function.
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ''
+  }`;
+
+  //! I wanna change my btnClasses to include the bump animation class, and then I wanna set a timer which removes that class again so that when it's added again in the future, it again plays.
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    //! if you return a function in useEffect, this will be called automatically as a cleanup function by React.
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
