3348f736272e00506c32f030e0471d02507d6137
Ger4eK <ger4ek228@gmail.com>
2022-03-27 11:16:07 +0300

    another way to loading and error handlers

--- src/components/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -35,9 +35,9 @@ const AvailableMeals = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [httpError, setHttpError] = useState();
 
-  useEffect(() => {
-    const fetchMealsData = async () => {
-      setIsLoading(true);
+  const fetchMealsData = async () => {
+    setIsLoading(true);
+    try {
       const response = await fetch(
         'https://react-http-9066a-default-rtdb.firebaseio.com/meals.json'
       );
@@ -60,14 +60,48 @@ const AvailableMeals = () => {
 
       setMeals(loadedMeals);
       setIsLoading(false);
-    };
-
-    //! it's a promise and add to catch method on it - fetchMealsData().catch(...)
-    fetchMealsData().catch((error) => {
+    } catch (error) {
       setIsLoading(false);
       setHttpError(error.message);
-    });
+    }
+  };
+  useEffect(() => {
+    fetchMealsData();
   }, []);
+
+  //useEffect(() => {
+  //  const fetchMealsData = async () => {
+  //    setIsLoading(true);
+  //    const response = await fetch(
+  //      'https://react-http-9066a-default-rtdb.firebaseio.com/meals.json'
+  //    );
+
+  //    if (!response.ok) {
+  //      throw new Error('Something went wrong!');
+  //    }
+
+  //    const data = await response.json();
+
+  //    const loadedMeals = [];
+  //    for (const key in data) {
+  //      loadedMeals.push({
+  //        id: key,
+  //        name: data[key].name,
+  //        description: data[key].description,
+  //        price: data[key].price,
+  //      });
+  //    }
+
+  //    setMeals(loadedMeals);
+  //    setIsLoading(false);
+  //  };
+
+  //  //! it's a promise and add to catch method on it - fetchMealsData().catch(...)
+  //  fetchMealsData().catch((error) => {
+  //    setIsLoading(false);
+  //    setHttpError(error.message);
+  //  });
+  //}, []);
 
   const mealsList = meals.map((meal) => (
     <MealItem
