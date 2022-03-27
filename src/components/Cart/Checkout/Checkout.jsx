f61ae1f2fb74ee86ea235d69414700fbba6225c3
Ger4eK <ger4ek228@gmail.com>
2022-03-27 12:58:49 +0300

    reading form values

--- src/components/Cart/Checkout/Checkout.jsx
+++ src/components/Cart/Checkout/Checkout.jsx
@@ -1,28 +1,37 @@
-import { React, useCallback } from 'react';
+import { React, useRef } from 'react';
 import classes from './Checkout.module.css';
 
 const Checkout = (props) => {
+  const nameInput = useRef();
+  const streetInput = useRef();
+  const postalInput = useRef();
+  const cityInput = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInput.current.value;
+    const enteredPostal = postalInput.current.value;
+    const enteredCity = cityInput.current.value;
   };
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' ref={nameInput} />
       </div>
       <div className={classes.control}>
         <label htmlFor='street'>Street</label>
-        <input type='text' id='street' />
+        <input type='text' id='street' ref={streetInput} />
       </div>
       <div className={classes.control}>
         <label htmlFor='postal'>Postal Code</label>
-        <input type='text' id='postal' />
+        <input type='text' id='postal' ref={postalInput} />
       </div>
       <div className={classes.control}>
         <label htmlFor='city'>City</label>
-        <input type='text' id='city' />
+        <input type='text' id='city' ref={cityInput} />
       </div>
       <div className={classes.actions}>
         <button type='button' onClick={props.onCancel}>
